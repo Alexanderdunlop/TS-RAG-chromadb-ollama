417db3b9aa05b20775bf5e091607a813c023a4c7
Alexanderdunlop <alexjamesdunlop@gmail.com>
2024-09-02 12:29:06 +0100

    add chromadb

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ To install dependencies:
 bun install
 ```
 
+To run Chroma:
+
+```bash
+chroma run --host localhost --port 8000 --path ./my_chroma_data
+```
+
 To run:
 
 ```bash
@@ -13,3 +19,5 @@ bun run index.ts
 ```
 
 This project was created using `bun init` in bun v1.1.26. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+
+- [chromadb](https://cookbook.chromadb.dev/running/running-chroma/)
--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import { ChromaClient } from "chromadb";
+
+const collectionName = "buildragwithts";
+
+const chroma = new ChromaClient({
+  path: "http://localhost:8000",
+});
+// NOTE: Normally you would not want to to this, but in this example we are deleting the collection
+await chroma.deleteCollection({
+  name: collectionName,
+});
+const collection = await chroma.getOrCreateCollection({
+  name: collectionName,
+});
+
+console.log("Collection", collection);
