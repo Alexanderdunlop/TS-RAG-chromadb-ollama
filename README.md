0755e6bb7a740ff8a31cc1462503a702dde24b32
Alexanderdunlop <alexjamesdunlop@gmail.com>
2024-09-02 12:54:05 +0100

    add ollama

--- README.md
+++ README.md
@@ -2,22 +2,35 @@
 
 To install dependencies:
 
+## Install
+
 ```bash
 bun install
 ```
 
 To run Chroma:
 
+## Run
+
+### Chroma
+
 ```bash
 chroma run --host localhost --port 8000 --path ./my_chroma_data
 ```
 
-To run:
+### Ollama
 
 ```bash
-bun run index.ts
+ollama run llama3.1
+```
+
+### Import
+
+```bash
+bun run import.ts
 ```
 
 This project was created using `bun init` in bun v1.1.26. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
 
 - [chromadb](https://cookbook.chromadb.dev/running/running-chroma/)
+- [ollama](https://github.com/ollama/ollama?tab=readme-ov-file)
---
+++ import.ts
@@ -0,0 +1,44 @@
+import { ChromaClient } from "chromadb";
+import { getConfig, readText } from "./utils";
+import { chunkTextBySentences } from "matts-llm-tools";
+import ollama from "ollama";
+
+const collectionName = "buildragwithts";
+const { embedmodel, mainmodel } = getConfig();
+
+const chroma = new ChromaClient({
+  path: "http://localhost:8000",
+});
+// NOTE: Normally you would not want to to this, but in this example we are deleting the collection
+await chroma.deleteCollection({
+  name: collectionName,
+});
+const collection = await chroma.getOrCreateCollection({
+  name: collectionName,
+});
+
+const docstoimport = (await Bun.file("sourcedocs.txt").text()).split("\n");
+
+for (const doc of docstoimport) {
+  console.log(`\nEmbedding chunks from ${doc}\n`);
+  const text = await readText(doc);
+  const chunks = chunkTextBySentences(text, 7, 2);
+
+  for await (const [index, chunk] of chunks.entries()) {
+    const embed = (
+      await ollama.embeddings({
+        model: embedmodel,
+        prompt: chunk,
+      })
+    ).embedding;
+    await collection.add({
+      ids: [doc + index],
+      embeddings: [embed],
+      metadatas: {
+        source: doc,
+      },
+      documents: [chunk],
+    });
+    process.stdout.write(".");
+  }
+}
